abb683e6522c06b22c34dcf135c8022fdfc38b13
Valet78 <valet78@mail.ru>
2023-02-10 22:51:26 +0300

    Итог 6 семинара

---
+++ sem_6/Point2d.java
@@ -0,0 +1,31 @@
+package sem_6;
+
+public class Point2d {
+    private int x = 0, y = 0;
+    private String sst = "0:0";
+    private String[] strIn = new String[2];
+
+    // Конвертация позиции в массиве (поле) в строку для очереди
+    public String GetStr(int inY, int inX){
+        this.x = inX;
+        this.y = inY;
+        return Integer.toString(y) + ":" + Integer.toString(x);
+    }
+
+    // Конвертация адреса из очереди в число
+    public int GetY(String adr){
+        this.sst = adr;
+        strMass();        
+        return Integer.parseInt(this.strIn[0]);        
+    }
+    
+    public int GetX(String adr){
+        this.sst = adr;
+        strMass();        
+        return Integer.parseInt(this.strIn[1]);
+    }
+
+    private void strMass(){
+        this.strIn = this.sst.split(":");
+    }    
+}
